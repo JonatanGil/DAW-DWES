adbf7dd31adba4054110c5704a18d858bb3dbc7c
jon Gil <jonyqro@gmail.com>
2019-09-26 16:44:26 +0200

    añado 3.1php

---
+++ 3.1is_int.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+
+<body>
+
+  <p>Before the script...</p>
+
+  <script>
+
+    $1=1;
+    $2="1";
+
+    echo is_int($1);
+    echo is_int($2);
+
+  </script>
+<br><br>
+  <p>...After the script.</p>
+
+</body>
+
+</html>
