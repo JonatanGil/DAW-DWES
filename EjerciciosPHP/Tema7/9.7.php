9a4f1f0c63cad1447bac5872f26f2211a7ddd768
jon Gil <jonyqro@gmail.com>
2020-01-17 18:53:14 +0100

    añado 9.7 mejorao

--- EjerciciosPHP/Tema7/9.7.php
+++ EjerciciosPHP/Tema7/9.7.php
@@ -36,72 +36,14 @@
            }else{
 
 
-
-        $servidor = "localhost";
-        $username = "miusuario";
-        $password = "mipassword";
-        $basedatos = "bdprueba";
-
         # Crear conexión
-        $conn = mysqli_connect($servidor, $username, $password, $basedatos);
-    
-        if (!$conn) {
-            die("Conexi&ocacuten fallida: " . mysqli_connect_error());
-        }
-        echo "Conexi&oacuten con &eacutexito <br><br>";
-
-        $dni = mysqli_query($conn, "SELECT MAX(dni) FROM ejercicio92;");
-        
-        if(!$dni){
-            echo "selector fallido<br>";
-        }  else{
-            echo "selector con exito<br>";
-        }
-
-
-        while ($fila = mysqli_fetch_array($dni)){ 
-            echo $fila[0];
-            $dniNumero=$fila[0];
-        }
-
-        //sumamos una al id dni
-        $dniNumero++;
-
-        $dniSumado= $dniNumero;
-        $nombreE = $_POST['nombre'];
-        $sueldo = $_POST["sueldo"];
-        $plus = $_POST["plus"];
-
-        $consulta = "INSERT INTO ejercicio92(dni,nombre,sueldo,plus) VALUES ($dniSumado,'$nombreE',$sueldo,$plus);";
-        echo $consulta."<br>";
-        $result2 = mysqli_query($conn,$consulta);
-        if ($result2==TRUE){
-            echo "Se ha insertado la fila: ".$dninumero."/ ".$nombreE."/ ".$sueldo."/ ".$plus ."<br>";
-        }else{
-            # La siguiente función muestra el último error, en caso
-            # de haberlo.
-            echo mysqli_error($conn);
-            die ("Hubo un error");
-        }   # Como no se trata de un SELECT, mysqli_query devuelve TRUE
-            
-
-
+        $conn = conexionBDD();
+        InsertarEmpleado($conn);
         mysqli_close($conn);
     }
 
-    $servidor = "localhost";
-    $username = "miusuario";
-    $password = "mipassword";
-    $basedatos = "bdprueba";
-
-    # Crear conexión
-    $conn = mysqli_connect($servidor, $username, $password, $basedatos);
-
-    if (!$conn) {
-        die("<br><br>Conexi&ocacuten fallida: " . mysqli_connect_error());
-    }
-    echo "<br><br>Conexi&oacuten con &eacutexito <br><br>";
-    
+    # Crear conexión y ver todos los dni
+    $conn = conexionBDD();
     $empleados = mysqli_query($conn, " SELECT * from ejercicio92;");
         
         if(!$empleados){
@@ -109,13 +51,6 @@
         }  else{
             echo "selector con exito<br>";
         }
-/*
-        while ($fila = mysqli_fetch_array($empleados)){ 
-            echo $fila[0];
-            $dniNumero=$fila[0];
-        }*/
-
-
     ?>
 
     <form action="9.7.php" method="POST">
@@ -138,36 +73,97 @@
            # En caso contrario (es decir, si se ha pulsado antes el botón)...
     }else{
 
+    # Crear conexión
+    $conn = conexionBDD();
+    //eliminar dni funcion
+    eliminarDNI($conn);
+    mysqli_close($conn);
+
+    }
+
+
+
+function InsertarEmpleado($conecxion){
+
+    $dni = mysqli_query($conecxion, "SELECT MAX(dni) FROM ejercicio92;");
+        
+    if(!$dni){
+        echo "selector fallido<br>";
+    }  else{
+        echo "selector con exito<br>";
+    }
+
+
+    while ($fila = mysqli_fetch_array($dni)){ 
+        echo $fila[0];
+        $dniNumero=$fila[0];
+    }
+
+    //sumamos una al id dni
+    $dniNumero++;
+
+    $dniSumado= $dniNumero;
+    $nombreE = $_POST['nombre'];
+    $sueldo = $_POST["sueldo"];
+    $plus = $_POST["plus"];
+
+    $consulta = "INSERT INTO ejercicio92(dni,nombre,sueldo,plus) VALUES ($dniSumado,'$nombreE',$sueldo,$plus);";
+    echo $consulta."<br>";
+    $result2 = mysqli_query($conecxion,$consulta);
+
+    if ($result2==TRUE){
+        echo "Se ha insertado la fila: ".$dniSumado."/ ".$nombreE."/ ".$sueldo."/ ".$plus ."<br>";
+    }else{
+        # La siguiente función muestra el último error, en caso
+        # de haberlo.
+        echo mysqli_error($conecxion);
+        die ("Hubo un error");
+    }   # Como no se trata de un SELECT, mysqli_query devuelve TRUE
+        
+
+
+}
+
+function eliminarDNI($conecxion){
+    $dniEliminar = $_POST["eliminar"];
+    echo "<br> DNI a eliminar".$dniEliminar.":".$_POST["eliminar"]."<br>";
+    $consulta = "DELETE FROM ejercicio92 WHERE DNI LIKE $dniEliminar;";
+    $resultado = mysqli_query($conecxion,$consulta);
+    if ($resultado==TRUE){
+        echo "Se ha eliminado el dni".$dniEliminar."<br>";
+    }else{
+        # La siguiente función muestra el último error, en caso
+        # de haberlo.
+        echo mysqli_error($conecxion);
+        die ("Hubo un error");
+    }   # Com
+    
+   
+}
+
+function conexionBDD(){
+
+
     $servidor = "localhost";
     $username = "miusuario";
     $password = "mipassword";
     $basedatos = "bdprueba";
 
     # Crear conexión
-    $conn = mysqli_connect($servidor, $username, $password, $basedatos);
+    $conecxion = mysqli_connect($servidor, $username, $password, $basedatos);
 
-    if (!$conn) {
+    if (!$conecxion) {
         die("<br><br>Conexi&ocacuten fallida: " . mysqli_connect_error());
     }
     echo "<br><br>Conexi&oacuten con &eacutexito <br>";
-    
-    $dniEliminar = $_POST["eliminar"];
-    echo "<br> DNI a eliminar".$dniEliminar.":".$_POST["eliminar"]."<br>";
-    $consulta = "DELETE FROM ejercicio92 WHERE DNI LIKE $dniEliminar;";
-    $resultado = mysqli_query($conn,$consulta);
-    if ($resultado==TRUE){
-        echo "Se ha eliminado el dni".$dniEliminar."<br>";
-    }else{
-        # La siguiente función muestra el último error, en caso
-        # de haberlo.
-        echo mysqli_error($conn);
-        die ("Hubo un error");
-    }   # Com
-    
-   
 
-    mysqli_close($conn);
+    if (!$conecxion) {
+        die("<br><br>Conexi&ocacuten fallida: " . mysqli_connect_error());
+    }
+    echo "<br><br>Conexi&oacuten con &eacutexito <br><br>";
+    
 
+        return $conecxion;
     }
 
 
